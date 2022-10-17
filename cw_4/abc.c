c425dac703a8d33d8f64c936f5627e81d2aadf42
Bogachevv <dog67688@gmail.com>
2022-10-18 00:26:27 +0300

    Contest 1

--- cw_4/abc.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	int i, j = 1, k = 9;
-	do while (i = k--);
-	printf("i = %d, j = %d, k = %d\n", i, j, k);
-	while (k=++i, j--);
-	printf("i = %d, j = %d, k = %d\n", i, j, k);
-}
