6145ef2f183dc94b1a2349ac8f7f48cf9b62673e
Bogachevv <dog67688@gmail.com>
2022-10-25 00:44:06 +0300

    Writing parser

--- shell/lex.c
+++ shell/lex.c
@@ -5,55 +5,57 @@
 #define TRUE 1
 #define FALSE 0
 
+void non_quotes_mode(char *ch_p, char **str_beg, char **str_end, int *state){
+	if (*ch_p == '"'){
+		*str_beg = ch_p + 1;
+		*state |= 1; //set last bit
+		return;
+	}
+	if (*ch_p == ' '){
+		*str_end = ch_p;
+		return;
+	}
+}
+
+void quotes_mode(char *ch_p, char **str_beg, char **str_end, int *state){
+	if (*ch_p != '"') return;
+	if (*(ch_p + 1) == ' '){
+		*str_end = ch_p + 1;
+		*state &= -2; //clear last bit
+	}
+}
+
+void spec_mode(char *ch_p, size_t cnt, char **str_beg, char **str_end){
+	*str_beg = ch_p;
+	*str_end = ch_p + cnt;
+}
+
+int char_in_str(char ch, const char *str){
+	for (size_t i = 0; str[i]; ++i) if (str[i] == ch) return TRUE;
+	return FALSE;
+}
+
 char **parse_input(char *str){
 	char **res_buf = NULL;
-	int res_buf_cap = 0;
-	int res_buf_len = 0;
-	int ch, ignore_space = FALSE;
-	int prev_sep = -1, i;
-	for (i = 0, ch = *str; str[i] != 0; ++i, ch = str[i]){
-		if ((ch == '&') || (ch == '|') || (ch == '>')){
-			if (i > 0){
-				int res_len = i - prev_sep;
-				char *res = malloc(res_len + 1);
-				strncpy(res, str + prev_sep + 1, res_len);
-				res[res_len] = 0;
-				prev_sep = i;
-				PUSH_BACK(res_buf, res_buf_cap, res_buf_len, res);
-			}
-			int ln = 1 + (str[i+1] == ch);
-			i += (str[i+1] == ch);
-			prev_sep = i;
-			char *res = malloc(ln + 1);
-			for (int j = 0; j < ln; ++j) res[j] = ch;
-			res[ln] = 0;
-			PUSH_BACK(res_buf, res_buf_cap, res_buf_len, res);
-			continue;
+	int state = 0;
+	char *str_beg = str, *str_end = str - 1;
+	for (char *ch_p = str; *ch_p; ++ch_p){
+		if (char_in_str(*ch_p, "&|;><()")){
+			size_t cnt = 1 + (*(ch_p + 1) == *ch_p);
+			spec_mode(ch_p, cnt, &str_beg, &str_end);
 		}
-		if ((ch == '"') && (str[i + 1] != ' ')){
-			ignore_space = TRUE;
-			continue;
-		}
-		if (((ch == ' ') && (!ignore_space)) || ((ch == '"') && (str[i + 1] == ' '))){
-			//store str
-			int res_len = i - prev_sep;
-			char *res = malloc(res_len + 1);
-			strncpy(res, str + prev_sep + 1, res_len);
-			res[res_len] = 0;
-			prev_sep = i;
-			PUSH_BACK(res_buf, res_buf_cap, res_buf_len, res);
-			i += (ch == '"');
-			prev_sep += (ch == '"');
-		}
-	}
-		
-	int res_len = i - prev_sep;
-	char *res = malloc(res_len + 1);
-	strncpy(res, str + prev_sep + 1, res_len);
-	res[res_len] = 0;
-	prev_sep = i;
-	PUSH_BACK(res_buf, res_buf_cap, res_buf_len, res);
+		if (state & 1) quotes_mode(ch_p, &str_beg, &str_end, &state);
+		else non_quotes_mode(ch_p, &str_beg, &str_end, &state);
 
-	PUSH_BACK(res_buf, res_buf_cap, res_buf_len, NULL);
+		if (str_end >= str_beg){
+			size_t str_len = str_end - str_beg + 1;
+			char *new_str = malloc(str_len + 1);
+			memcpy(new_str, str_beg, str_len);
+			new_str[str_len] = 0;
+			printf("Parse res: %s\n", new_str);
+			str_beg = ch_p + 1;
+		}	
+	}
+
 	return res_buf;
 }
--- shell/main.c
+++ shell/main.c
@@ -1,15 +1,8 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include "lex.h"
 
-int readline(char *buf, int *buf_cap, FILE *fd){
-	int ch;
-	while ((ch = getchar()) != EOF){
-		if (ch == '\n') break;
-		
-	}
-
-}
 
 int main(int argc, char** argv){
 	//char* str = "ABCDEF ABCDEF ABCDEF||ABCDEF \"Hello world\" mir";
@@ -17,10 +10,10 @@ int main(int argc, char** argv){
 	while (!feof(stdin)){
 		char *str = NULL; size_t len;
 		len = getline(&str, &len, stdin);
-		printf("Line [%d]: %s\n", len, str);
+		printf("Line [%ld]: %s\n", len, str);
 		char **parsed = parse_input(str);
-		for (int i = 0; parsed[i]; ++i){
-			printf("%s\n", parsed[i]);
-		}
+		//for (int i = 0; parsed[i]; ++i){
+		//	printf("%s\n", parsed[i]);
+		//}
 	}
 }
