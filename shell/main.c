66af92b74a5d04788c4c54caa9b4eedc6ba94507
Bogachevv <dog67688@gmail.com>
2022-10-31 12:47:04 +0300

    Start writing part 2

--- shell/main.c
+++ shell/main.c
@@ -10,17 +10,41 @@ void free_parsed(char **parsed){
 }
 
 
+int run_cmd(char **args){
+	char *cmd = args[0];
+	if (strncmp(cmd, "cd", 2) == ){
+		char *pwd = args[1];
+		if (pwd == NULL) pwd = getenv("HOME");
+		int status = chdir(pwd);
+		if (status == -1){
+			//error handling
+			switch (errno){
+				case EACCES:
+					printf("Not enougth permissions to acces %s\n", pwd);
+					break;
+				case EFAULT:
+					printf("Incorrect path: %s\n", pwd);
+					break;
+				case ENAMETOOLONG:
+					printf("Path is too long:\n\tPath len: %d\tMax path len: %d", strlen(path), PATH_MAX);
+					break;
+				default:
+					printf("Unexpected error: %d\n", errno);
+			}
+		}
+	}
+
+}
+
+
 int main(int argc, char** argv){
 	char *str = NULL; size_t str_cap = 0, len = 0;
 	while (!feof(stdin)){
-		//printf(">>");
+		printf(">>");
 		len = getline(&str, &str_cap, stdin);
 		if (len == -1) break;
-		//printf("Line [%ld]: %s\n", len, str);
 		char **parsed = parse_input(str);
-		for (int i = 0; parsed[i]; ++i){
-			printf("%s\n", parsed[i]);
-		}
+		int run_status = run_cmd(parsed);
 		free_parsed(parsed);
 	}
 	free(str);
