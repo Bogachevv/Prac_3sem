299d0986ebd7a3c17b54e9e9d3965486d2a8e435
Bogachevv <dog67688@gmail.com>
2022-10-31 10:35:22 +0300

    Fix memory leaks

--- shell/main.c
+++ shell/main.c
@@ -1,20 +1,27 @@
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include "lex.h"
 
+void free_parsed(char **parsed){
+	for (char **str_ptr = parsed; *str_ptr; ++str_ptr){ 
+		free(*str_ptr);
+	}
+	free(parsed);
+}
+
 
 int main(int argc, char** argv){
-	//char* str = "ABCDEF ABCDEF ABCDEF||ABCDEF \"Hello world\" mir";
-	//printf("%s\n", str);
+	char *str = NULL; size_t str_cap = 0, len = 0;
 	while (!feof(stdin)){
-		char *str = NULL; size_t len;
-		len = getline(&str, &len, stdin);
+		printf(">>");
+		len = getline(&str, &str_cap, stdin);
 		if (len == -1) break;
 		printf("Line [%ld]: %s\n", len, str);
 		char **parsed = parse_input(str);
 		for (int i = 0; parsed[i]; ++i){
 			printf("%s\n", parsed[i]);
 		}
+		free_parsed(parsed);
 	}
+	free(str);
 }
