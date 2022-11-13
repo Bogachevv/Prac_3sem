19beff84210ffdf2c8c6934b81c9b8d83070cb44
Bogachevv <dog67688@gmail.com>
2022-11-13 22:18:42 +0300

    Add redirection, start writing async run

---
+++ shell/cmdrun.c
@@ -0,0 +1,202 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <signal.h>
+#include <fcntl.h>
+
+#include "utils.h"
+#include "cmdrun.h"
+
+size_t get_argc(char **argv){
+	size_t c = 0;
+	for (char **arg_p = argv; *arg_p; ++arg_p) ++c;
+	return c;
+}
+
+void redirect(char ***args_p, cmd_t *cmd){
+	char **arg_p = *args_p;
+	if (*(arg_p + 1) == NULL){
+		fprintf(stderr, "Not enougth args\n");
+	}
+
+	if ((*arg_p)[0] == '<'){ //stdin
+		int fd = open(*(arg_p + 1), O_RDONLY);
+		if (fd == -1){
+			fprintf(stderr, "Can't redirect %s to stdin\n", *(arg_p + 1));
+		}
+		cmd->inp_ph = fd;
+		++arg_p;
+	}
+	else 
+	if ((*arg_p)[0] == '>'){ 
+		int fd;
+		if ((*arg_p)[1] == '>') fd = open(*(arg_p + 1), O_WRONLY | O_APPEND);
+		else fd = open(*(arg_p + 1), O_WRONLY | O_CREAT | O_TRUNC, 0666);
+		
+		if (fd == -1){
+			fprintf(stderr, "Can't redirect stdout to %s\n", *(arg_p + 1));
+		}
+		cmd->out_ph = fd;
+		++arg_p;
+	}
+	*args_p = arg_p;
+}
+
+cmd_t *prepare_cmd(char **args, cmd_t *cmd){
+	if (cmd == NULL) cmd = calloc(1, sizeof(cmd_t));
+	cmd->path = args[0];
+	if (cmd->args != NULL) free(cmd->args);
+	cmd->args = calloc(get_argc(args) + 1, sizeof(char*));
+	cmd->mode = CMD_DEFAULT;
+		
+	cmd->inp_ph = 0;
+	cmd->out_ph = 1;
+	cmd->err_ph = 2;
+	
+	char **arg_wr = cmd->args;
+	for (char **arg_p = args; *arg_p; ++arg_p){
+		if (strchr("<>", (*arg_p)[0])){
+			redirect(&arg_p, cmd);
+		}
+		else if (((*arg_p)[0] == '&') && ((*arg_p)[1] != '&')){
+			cmd->mode = CMD_ASYNC;
+		}
+		else{
+			*arg_wr = *arg_p;
+			++arg_wr;
+		}
+
+	}
+	
+	cmd->next = NULL;
+	return cmd;
+}
+
+void free_cmd(cmd_t *cmd){
+	if (cmd == NULL) return;
+	free(cmd->args);
+	free_cmd(cmd->next);
+	free(cmd);
+}
+
+int check_file(char *dir, char *fname){
+	char path[PATH_MAX];
+	sprintf(path, "%s/%s", dir, fname);
+	FILE *fd = fopen(path, "r");
+	if (fd == NULL) 
+		return 0;
+	else{
+		fclose(fd);
+		return 1;
+	}
+}
+
+int get_full_path(char *cmd, char *buf, size_t buf_cap){
+	if ((cmd[0] == '.') || (cmd[0] == '/')) {
+		strncpy(buf, cmd, PATH_MAX);
+		return 0;
+	}
+	char *PATH = getenv("PATH");
+	char *PATH_END = PATH + strlen(PATH);
+	char *str_beg = PATH, *str_end;
+	char dir_path[PATH_MAX];
+	int f_check = 0;
+	do{
+		str_end = strstr(str_beg, ":");
+		str_end = (str_end) ? str_end : PATH_END;
+		size_t str_len = str_end - str_beg;
+		memcpy(dir_path, str_beg, str_len);
+		dir_path[str_len] = 0;
+		f_check = check_file(dir_path, cmd);
+		str_beg = str_end + 1;
+
+	} while ((str_end != PATH_END) && (!f_check));
+	
+	if (!f_check) return -1;
+	if (buf_cap < strlen(dir_path) + strlen(cmd) + 1){
+		printf("Size error: can't copy full path to the buffer \n(buf size = %ld)\n", buf_cap);
+		return -1;
+	}
+	sprintf(buf, "%s/%s", dir_path, cmd);
+	return 0;
+}
+
+
+int change_fd(int old_fd, int new_fd){
+	if (old_fd == new_fd) return 0;
+	//close(old_fd);
+	int state = dup2(new_fd, old_fd);
+	close(new_fd);
+	return state;
+}
+
+void parse_status(int status, int *usr_code, int *sys_code){
+	*usr_code = 0; *sys_code = 0;
+	if (status == 0) return;
+	
+	if (WIFEXITED(status))
+		*usr_code = WEXITSTATUS(status);
+	else
+		*sys_code = WTERMSIG(status);
+}
+
+void chld_handler(int sig){
+	int status;
+	wait(&status);
+	printf("Async finish status: %d\n", status);
+}
+
+int run_cmd(const cmd_t *cmd){
+	printf("CMD mode: %d\n", cmd->mode);
+	if (strncmp(cmd->path, "cd", 2ul) == 0){
+		return cd(cmd->args[1]);
+	}
+
+	if (strcmp(cmd->path, "exit") == 0){
+		return EXIT_C;
+	}
+	
+	pid_t pid = fork();
+	if (pid == -1){
+		printf("Fork error: errno%d\n", errno);
+		return -1;
+	}
+	if (pid){
+		int status = 0;
+		if (cmd->mode != CMD_ASYNC){
+			waitpid(pid, &status, 0);
+			int usr_code, sys_code;
+			parse_status(status, &usr_code, &sys_code);
+			printf("Exit status(usr, sys): %d, %d\n", usr_code, sys_code);
+			return status;
+		}
+		else {
+			signal(SIGCHLD, chld_handler);
+			return 0;
+		}
+	}
+	else{
+		char prog_path[PATH_MAX];
+		int rs = get_full_path(cmd->path, prog_path, PATH_MAX);
+		if (rs != 0){
+			fprintf(stderr, "Can't find %s\n", cmd->path);
+			exit(127);
+		}
+		printf("Running %s\n", prog_path);
+		
+		change_fd(0, cmd->inp_ph);	
+		change_fd(1, cmd->out_ph);	
+		change_fd(2, cmd->err_ph);	
+	
+		execv(prog_path, cmd->args);
+		printf("Execv error: %d\n", rs);
+		exit(rs);
+	}
+}
+
---
+++ shell/cmdrun.h
@@ -0,0 +1,23 @@
+#define EXIT_C -2
+
+#define CMD_ASYNC -1
+#define CMD_DEFAULT 0
+#define CMD_ON_SUCCES 1
+#define CMD_ON_ERROR 2
+
+typedef struct cmd{
+	char *path;
+	char **args;
+	int inp_ph, out_ph, err_ph;
+	struct cmd *next;
+	int mode;
+} cmd_t;
+
+cmd_t *prepare_cmd(char **args, cmd_t *cmd);
+
+int change_fd(int old_fd, int new_fd);
+
+void parse_status(int status, int *usr_code, int *sys_code);
+
+int run_cmd(const cmd_t *cmd);
+
